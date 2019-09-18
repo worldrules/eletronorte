3cc2e2c40fbef9dbca38b068e20492f87cdce016
worldrulesdev <worldrulesdev@gmail.com>
2019-09-18 17:37:21 -0300

    instalação do firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+      apiKey: 'AIzaSyDDsVfntsZQLrdxANP6Oks_4MfyZkPKDpE',
+      authDomain: 'eletronorte-4ce5b.firebaseapp.com',
+      databaseURL: 'https://eletronorte-4ce5b.firebaseio.com',
+      projectId: 'eletronorte-4ce5b',
+      storageBucket: '',
+      messagingSenderId: '196871475657',
+      appId: '1:196871475657:web:b468b19b754344a97e1818'
+    }
 };
