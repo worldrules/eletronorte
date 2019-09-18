c3280c4c2eafb913bdfa06824303ad63d0d8dde3
Augusto Santos <augusto.santos@hammerconsult.com.br>
2019-09-18 14:28:44 -0300

    Navbar e ícones da sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,19 +10,19 @@ export interface RouteInfo {
 
 export const ROUTES: RouteInfo[] = [
     { path: '/principal',     title: 'Principal',         icon:'nc-bank',       class: '' },
-    { path: '/dadosPessoais',     title: 'Dados Pessoais',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/atribuicao',     title: 'Atribuição Organizacional',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/endereco',     title: 'Endereço',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/deficiencia',     title: 'Deficiência',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/documentos',     title: 'Documentos',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/dadosBancarios',     title: 'Dados Bancários',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/familia',     title: 'Familia/Pessoa de Referência',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/contrato',     title: 'Elementos do Contrato',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/aposentadoria',     title: 'Aposentadoria',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/formacao',     title: 'Formaçao Educacional',         icon:'nc-bullet-list-67',       class: '' },
-    { path: '/dadosInternos',     title: 'Dados Internos da Empresa',         icon:'nc-bullet-list-67',       class: '' },
+    { path: '/dadosPessoais',     title: 'Dados Pessoais',         icon:'nc-single-02',       class: '' },
+    { path: '/atribuicao',     title: 'Atribuição Organizacional',         icon:'nc-minimal-right',       class: '' },
+    { path: '/endereco',     title: 'Endereço',         icon:'nc-minimal-right',       class: '' },
+    { path: '/deficiencia',     title: 'Deficiência',         icon:'nc-minimal-right',       class: '' },
+    { path: '/documentos',     title: 'Documentos',         icon:'nc-single-copy-04',       class: '' },
+    { path: '/dadosBancarios',     title: 'Dados Bancários',         icon:'nc-bank',       class: '' },
+    { path: '/familia',     title: 'Familia/Pessoa de Referência',         icon:'nc-minimal-right',       class: '' },
+    { path: '/contrato',     title: 'Elementos do Contrato',         icon:'nc-paper',       class: '' },
+    { path: '/aposentadoria',     title: 'Aposentadoria',         icon:'nc-minimal-right',       class: '' },
+    { path: '/formacao',     title: 'Formaçao Educacional',         icon:'nc-hat-3',       class: '' },
+    { path: '/dadosInternos',     title: 'Dados Internos da Empresa',         icon:'nc-briefcase-24',       class: '' },
     { path: '/user',          title: 'Cadastrar',      icon:'nc-badge',  class: '' },
-    // { path: '/mudeAquiParaCriarNovaRota',     title: 'mudeAquiParaCriarNovaRota',         icon:'nc-bullet-list-67',       class: '' },
+    // { path: '/mudeAquiParaCriarNovaRota',     title: 'mudeAquiParaCriarNovaRota',         icon:'nc-minimal-right',       class: '' },
 
 
 
